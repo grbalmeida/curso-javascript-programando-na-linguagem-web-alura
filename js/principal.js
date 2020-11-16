8d08af5c260477c37b7ad4806c53f563f86dcc60
gilvan <g.r.almeida@live.com>
2020-11-16 14:24:05 -0300

    Replicando a validação e o cálculo do IMC para todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,31 +1,36 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista';
 
-var paciente = document.querySelector('#primeiro-paciente');
-var tdPeso = paciente.querySelector('.info-peso');
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdAltura = paciente.querySelector('.info-altura');
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector('.info-imc');
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 1000) {
-    console.log('Peso inválido!');
-    pesoEhValido = false;
-    tdImc.textContent = 'Peso inválido!';
-}
+    var tdImc = paciente.querySelector('.info-imc');
 
-if (altura <= 0 || altura >= 3) {
-    console.log('Altura inválida!');
-    alturaEhValida = false;
-    tdImc.textContent = 'Altura inválida!';
-}
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-if (pesoEhValido && alturaEhValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if (peso <= 0 || peso >= 1000) {
+        console.log('Peso inválido!');
+        pesoEhValido = false;
+        tdImc.textContent = 'Peso inválido!';
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        console.log('Altura inválida!');
+        alturaEhValida = false;
+        tdImc.textContent = 'Altura inválida!';
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
