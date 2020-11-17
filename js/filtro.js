b829325978b33642f658da830df31ca43df29467
gilvan <g.r.almeida@live.com>
2020-11-17 07:26:58 -0300

    Implementando a lógica de filtragem

--- js/filtro.js
+++ js/filtro.js
@@ -5,6 +5,13 @@ campoFiltro.addEventListener('input', function(event) {
     for (var i = 0; i < pacientes.length; i++) {
         var paciente = pacientes[i];
         var tdNome = paciente.querySelector('.info-nome');
-        var nome = paciente.textContent;
+        var nome = tdNome.textContent.toLowerCase().trim();
+        var nomeDigitado = this.value.toLowerCase().trim();
+
+        if (!nome.includes(nomeDigitado)) {
+            paciente.classList.add('invisivel');
+        } else {
+            paciente.classList.remove('invisivel');
+        }
     }
 });
