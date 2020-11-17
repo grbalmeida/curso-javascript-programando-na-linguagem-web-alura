24b5faf690d39918c9a2f72ed19b33dcf6a211eb
gilvan <g.r.almeida@live.com>
2020-11-17 07:36:03 -0300

    Filtrando com expressão regular

--- js/filtro.js
+++ js/filtro.js
@@ -1,14 +1,15 @@
 var campoFiltro = document.querySelector('#filtrar-tabela');
-var pacientes = document.querySelectorAll('.paciente');
 
-campoFiltro.addEventListener('input', function(event) {
+campoFiltro.addEventListener('input', function() {
+    var pacientes = document.querySelectorAll('.paciente');
+
     for (var i = 0; i < pacientes.length; i++) {
         var paciente = pacientes[i];
         var tdNome = paciente.querySelector('.info-nome');
-        var nome = tdNome.textContent.toLowerCase().trim();
-        var nomeDigitado = this.value.toLowerCase().trim();
+        var nome = tdNome.textContent.trim();
+        var expressao = new RegExp(this.value.trim(), 'i');
 
-        if (!nome.includes(nomeDigitado)) {
+        if (!expressao.test(nome)) {
             paciente.classList.add('invisivel');
         } else {
             paciente.classList.remove('invisivel');
